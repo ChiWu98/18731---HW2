20a53c000b3e6a5b9361589ff137fe91570f95ef
ChiWu98 <ubuntu@ip-172-31-5-90.us-west-2.compute.internal>
2022-03-16 22:09:57 +0000

    4.2 1st

--- shrew_client.sh
+++ shrew_client.sh
@@ -3,3 +3,11 @@
 echo running iperf-client
 
 #TODO: add your code
+
+
+for i in {1..200}
+do
+    iperf -c 10.0.0.1 -p 5006  -t 0.1 -u -b 10M &
+    #iperf -c 10.0.0.1 -u -b 10M -p 5006 -t 0.25
+    sleep 0.9
+done
