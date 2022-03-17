a672d352b704fe12d18fc704b6c62fecccff26f2
ChiWu98 <ubuntu@ip-172-31-5-90.us-west-2.compute.internal>
2022-03-17 00:18:05 +0000

    Merge Change

--- dumbbell.py
+++ dumbbell.py
@@ -107,9 +107,7 @@ def bbnet():
     #TODO: Add yoour code to start long lived TCP flows
     hl1, hl2, hr1, hr2, a1, a2 = net.get('hl1', 'hl2','hr1', 'hr2','a1','a2')
 
-<<<<<<< HEAD
     #TODO: Add yoour code to start long lived TCP flows 
-<<<<<<< HEAD
     
     hl1, hl2, hr1, hr2, a1, a2  = net.get('hl1', 'hl2','hr1', 'hr2','a1','a2')
     
@@ -119,22 +117,15 @@ def bbnet():
 
 
     a2.cmd("iperf -c 10.0.0.1 -p 5003  -t 500 -i 1 &")
-=======
     hl1, hl2, hr1, hr2 = net.get('hl1', 'hl2','hr1', 'hr2')
     
-=======
->>>>>>> e411c8c16118d7412a41befe5bb4416576d721c6
     hl1.cmd("iperf -s -p 5001 -t 500 &")
     hl2.cmd("iperf -s -p 5002 -t 500 &")
-<<<<<<< HEAD
     
->>>>>>> 08c7f51ba12a7711d270171692984835361a305f
-=======
     a1.cmd("iperf -s -p 5003 -t 500 &")
 
 
     a2.cmd("iperf -c 10.0.0.1 -p 5003  -t 500 -i 1 &")
->>>>>>> e411c8c16118d7412a41befe5bb4416576d721c6
     hr1.cmd("iperf -c 10.0.0.3 -p 5001 -t 500 -i 1 &")
     hr2.cmd("iperf -c 10.0.0.4 -p 5002 -t 500 -i 1 &")
 
